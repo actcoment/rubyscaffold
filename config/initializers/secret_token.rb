2eaf275055996b2e0debdcf8be6fdea17fd4afc4
EC2 Default User <ec2-user@ip-172-31-11-124.us-east-2.compute.internal>
2018-02-02 23:06:58 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ppxpapp::Application.config.secret_key_base = '0360a4186e38d3d4c3204c4d87322addaf182c027be347ea1a631f2bd2cbfd75011575dfaaafcac117637b3fc498ed9cfd75d88eec06d8c1bbf5fa0825f8c431'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ppxpapp::Application.config.session_store :cookie_store, key: '_ppxpapp_session'
